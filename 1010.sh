668dc74cae33997e99b6ab26d4f3d3b921c9a132
jaz <jazhten@github.com>
2018-11-13 22:12:19 +0800

    -a

---
+++ 1010.sh
@@ -0,0 +1,7 @@
+set -e
+
+sourcename=$1
+
+clang -g -Wall -I../c/library/include -L../c/library/lib $1 -lcs1010 -lm
+echo "Compiled successfully, running $1"
+./a.out
---
+++ test.c
@@ -0,0 +1,7 @@
+#include "cs1010.h"
+
+int main()
+{
+    cs1010_println_string("Test");
+    return 0;
+}
